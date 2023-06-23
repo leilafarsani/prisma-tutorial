f9a9e8c3a2bd2119a3fa64d9296c9563134b5e4c
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-06-23 23:50:15 +0100

    script.ts

--- script.ts
+++ script.ts
@@ -1,2 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+//prisma.user.findFirst
+
+// writing a little bit of code here 
+
+async function main(){
+    //... we will write our Prisma Client queries here.
+}
+main()
+.catch(e=> {
+    console.error(e.message)
+})
+.finally(async()=>{
+    await prisma.$disconnect()
+})
